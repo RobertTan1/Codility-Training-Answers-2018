75f1c6b70c08766c2880523d328e4e78f724e7ff
Robert <ziomas1@hotmail.com>
2018-08-18 15:37:59 -0400

    update

--- TapeEquilibrium.js
+++ TapeEquilibrium.js
@@ -1,7 +1,20 @@
 function solution(A) {
-    subt = []
-    for(var i = 1; i<A.length; i++) {
-        B = A.slice(0,i)
-        C = A.slice(i)
+    B = []
+    
+    C = []
+    
+    A.reduce(function(a,b,i) { return B[i] = a+b; },0);
+    
+    A.reverse().reduce(function(a,b,i) { return C[i] = a+b; },0);
+    
+    C.reverse()
+    
+    console.log(B,C)
+    diff = []
+    
+    for (var i = 0; i< A.length-1;i++) {
+        diff[i] = Math.abs(B[i]-C[i+1])
     }
+
+    return Math.min(...diff)
 }
