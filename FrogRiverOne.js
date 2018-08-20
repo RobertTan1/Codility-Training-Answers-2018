338929080d18720cc7be538a81136ea9ecd6b24c
Robert <ziomas1@hotmail.com>
2018-08-19 22:11:46 -0400

    slow solution

--- FrogRiverOne.js
+++ FrogRiverOne.js
@@ -1,18 +1,13 @@
-// you can write to stdout for debugging purposes, e.g.
-// console.log('this is a debug message');
-
 function solution(X, A) {
-    notFound = true
-    searchIndex = A.slice()
-    while (notFound) {
-        position = searchIndex.indexOf(X)
-        B = Array.from(Array(X).keys()).slice(1);
-        C = Array.from(new Set(A.slice(0,position)))
-        if (B.length <= C.length) {
-            notFound = false
-            return(position)
-        } else {
-            searchIndex[position] = 2147483647
+    
+    if (A.length == 1) {
+        return -1
+    }
+    
+    for (var i in A) {
+        if (Array.from(new Set(A.slice(0,i))).length === X) {
+            return parseInt(i-1)
         }
     }
+    return -1
 }
