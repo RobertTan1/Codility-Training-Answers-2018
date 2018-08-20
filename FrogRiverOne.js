800015a2a853f97eca36b88eb4363cbe8c9ffa71
Robert <ziomas1@hotmail.com>
2018-08-19 22:17:07 -0400

    fix

--- FrogRiverOne.js
+++ FrogRiverOne.js
@@ -1,12 +1,8 @@
 function solution(X, A) {
     
-    if (A.length == 1) {
-        return -1
-    }
-    
     for (var i in A) {
-        if (Array.from(new Set(A.slice(0,i))).length === X) {
-            return parseInt(i-1)
+        if (Array.from(new Set(A.slice(0,i+1))).length === X) {
+            return parseInt(i)
         }
     }
     return -1
