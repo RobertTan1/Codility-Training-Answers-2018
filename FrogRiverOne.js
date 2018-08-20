f9ed62f17fd3f9f0fb033ec88249c2c93a0be578
Robert <ziomas1@hotmail.com>
2018-08-19 22:32:55 -0400

    not working, in progress

--- FrogRiverOne.js
+++ FrogRiverOne.js
@@ -2,7 +2,7 @@ function solution(X, A) {
     
     for (var i in A) {
         if (Array.from(new Set(A.slice(0,i+1))).length === X) {
-            return parseInt(i)
+            return parseInt(i-1)
         }
     }
     return -1
