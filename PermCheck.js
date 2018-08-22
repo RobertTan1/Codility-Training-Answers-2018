2d683091be8188f5a89db44e14053d02b1a271d5
Robert <ziomas1@hotmail.com>
2018-08-22 19:46:06 -0400

    100%

--- PermCheck.js
+++
@@ -1,6 +0,0 @@
-function solution(A) {
-    if (JSON.stringify(A.sort()) === JSON.stringify([...Array(A.length + 1).keys()].slice(1))) {
-        return 1
-    }
-    return 0
-}
---
+++ PermCheck.py
@@ -0,0 +1,15 @@
+def solution(A):
+    if set(A) == set(range(1, 1+len(A))):
+        return 1
+    else:
+        return 0
+
+// For some reason the javascript version gives only 75% score while being conceptually the same
+
+function solution(A) {
+    trueLen = Array.from(new Set(A)).length + 1
+    if (JSON.stringify(Array.from(new Set(A)).sort()) === JSON.stringify([...Array(trueLen).keys()].slice(1))) {
+        return 1
+    }
+    return 0
+}
