5ed50c6ee2fabe526dc985c0b50b98d1fac33ce9
Robert <ziomas1@hotmail.com>
2018-08-22 19:20:28 -0400

    75%

--- PermCheck.js
+++ PermCheck.js
@@ -1,8 +1,6 @@
 function solution(A) {
-    B = Array.from(new Set(A)).sort()
-    if (B[B.length-1] == B.length && B.length == A.length && B[0] == 1) {
+    if (JSON.stringify(A.sort()) === JSON.stringify([...Array(A.length + 1).keys()].slice(1))) {
         return 1
-    } else {
-        return 0
     }
+    return 0
 }
