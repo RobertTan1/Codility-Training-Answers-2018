e061ced47935a996e868518c22bb7b0b277d7156
Robert <ziomas1@hotmail.com>
2018-08-24 14:21:50 -0400

    100%

--- PassingCars.js
+++ PassingCars.js
@@ -1,4 +1,21 @@
 function solution(A) {
+    zeroCount = 0
+    crossCount = 0
+    for (var i = 0; i < A.length; i++) {
+        if (A[i] == 0) {
+            zeroCount++
+        } else {
+            crossCount += zeroCount
+        }
+    }
+    if (crossCount > 1000000000) {
+        return -1
+    }
+    return crossCount
+} 
+
+// Slow solution
+function solution(A) {
     count = 0;
     
     for (var i in A) {
