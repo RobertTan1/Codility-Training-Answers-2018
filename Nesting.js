c32d430f562487465cae1c03f1903bcc73d2d719
Robert <ziomas1@hotmail.com>
2018-08-24 16:35:16 -0400

    100%

--- Nesting.js
+++ Nesting.js
@@ -1,5 +1,25 @@
 function solution(S) {
     S = S.split("")
+    counts = [0];
+    for (var i = 0; i < S.length; i++) {
+        if (S[i] =="(") {
+            counts.push(1)
+        } else {
+            counts.pop()
+            if (counts.length == 0) {
+                return 0
+            }
+        }
+    }
+    if (counts.length == 1) {
+        return 1
+    }
+    return 0
+}
+
+// Slow solution, previous
+function solution(S) {
+    S = S.split("")
     runCount = 0
     for (var i in S) {
         if (S[i] == "(") {
